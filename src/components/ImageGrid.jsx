3c483579d78c47d83c22247a882380a74b0e5e62
Noah Bresler <noahbresler2002@gmail.com>
2020-08-01 05:56:28 -0500

    feat: create ImageGrid component

---
+++ src/components/ImageGrid.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+export default function ImageGrid() {
+  // useEffect(() => {
+  //   fetch(
+  //     "https://res.cloudinary.com/doxbv70hu/image/list/poglib.json"
+  //   ).then((response) => console.log(response.data));
+  // });
+
+  return <div>Testtest</div>;
+}
