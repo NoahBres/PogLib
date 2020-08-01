b58cc8a8872c5825c658404c852a4f6fd05c2d15
Noah Bresler <noahbresler2002@gmail.com>
2020-08-01 05:57:16 -0500

    feat: add UploadWidget and ImageGrid to main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import UploadWidget from "./components/UploadWidget.jsx";
+import ImageGrid from "./components/ImageGrid.jsx";
+
 export default function App() {
-  return <div>Test Poglib</div>;
+  return (
+    <div>
+      <h1>Test</h1>
+      <UploadWidget />
+      <ImageGrid />
+    </div>
+  );
 }
