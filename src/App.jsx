b66137f40187a1ccd77e4fcbb0ad4c196c2b1ce4
Noah Bresler <noahbresler2002@gmail.com>
2020-08-03 04:39:49 -0500

    feat: add background color to main page

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import MainGrid from "./components/MainGrid.jsx";
 
 export default function App() {
   return (
-    <>
+    <div className="bg-gray-900">
       <MainGrid />
-    </>
+    </div>
   );
 }
